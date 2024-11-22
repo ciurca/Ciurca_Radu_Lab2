81cb780080ac773a29b88e0151dc3e49f51ad03e
Radu Ciurca <raduciurca@gmail.com>
2024-11-22 20:16:27 +0200

    Sarcina lab 3 rezolvata

--- Models/Authors.cs
+++ Models/Authors.cs
@@ -9,5 +9,6 @@ namespace Ciurca_Radu_Lab2.Models
         public string LastName { get; set; }
         [NotMapped]
         public string FullName { get { return FirstName + " " + LastName; } }
+        public ICollection<Book>? Books { get; set; }
     }
 }
